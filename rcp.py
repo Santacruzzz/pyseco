a17c69556a6ba97fecb1cf361225616562d8b9fc
Tomek <tomekm147@gmail.com>
2020-04-26 13:19:35 +0200

    Added passing debug arguments

--- rcp.py
+++ rcp.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from utils import *
 from pyseco import Pyseco
 from player_listener import PlayerListener
+import argparse
+
+parser = argparse.ArgumentParser(description='Pyseco. Trackmania Server Control')
+parser.add_argument('-v', '--verbose', default=1, type=int, help='Logging mode. 0 - DEBUG, 1 - INFO, 9 - NONE')
+parser.add_argument('-c', '--color', default='f90', type=str, help='Debug color')
+
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    pyseco = Pyseco('SuperAdmin', 'optimus1', '86.106.91.148', 5002, DEBUG)
+    pyseco = Pyseco('SuperAdmin', 'optimus1', '86.106.91.148', 5002, args.verbose)
+    pyseco.set_debug_data({'color': args.color})
     pyseco.register_listener(PlayerListener('PlrLstnr'))
     pyseco.run()
