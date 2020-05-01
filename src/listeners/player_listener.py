459c0bf33d7d3137fc2cad093600e0f49c3bc20d
Tomek <tomekm147@gmail.com>
2020-05-01 19:46:46 +0200

    Extracted transport logic to Transport class and simplified events handling

--- src/listeners/player_listener.py
+++ src/listeners/player_listener.py
@@ -11,20 +11,19 @@ class PlayerListener(Listener):
         super(PlayerListener, self).__init__(name, pyseco_instance)
         self.pyseco.register(EventPlayerConnect.name, self.on_player_connect)
         self.pyseco.register(EventPlayerDisconnect.name, self.on_player_disconnect)
-        self.pyseco.register(EventPlayerCheckpoint.name, self.on_player_checkpoint)
+        self.pyseco.register(EventPlayerCheckpoint.name, self.on_player_cp)
 
     def on_player_connect(self, data: EventPlayerConnect):
         login, is_spectator = data.login, data.is_spectator
         self.pyseco.add_player(login, is_spectator)
         player = self.pyseco.get_player(login)
-        self.pyseco.client.server_message(f'{strip_size(player.info.nickname)}$z$s$888 has joined')
+        self.pyseco.server_message(f'{strip_size(player.info.nickname)}$z$s$888 has joined')
 
     def on_player_disconnect(self, data: EventPlayerDisconnect):
         login = data.login
         player = self.pyseco.get_player(login)
-        self.pyseco.client.server_message(f'{strip_size(player.info.nickname)}$z$s$888 has left')
+        self.pyseco.server_message(f'{strip_size(player.info.nickname)}$z$s$888 has left')
         self.pyseco.remove_player(login)
 
-    def on_player_checkpoint(self, data: EventPlayerCheckpoint):
-        player = self.pyseco.get_player(data.login)
-        self.pyseco.client.server_message(f'{strip_size(player.info.nickname)}$z$s$888 on cp')
+    def on_player_cp(self, data: EventPlayerCheckpoint):
+        pass
