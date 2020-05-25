2600fa8ad023a213022153239114a75fca521560
Tomek <tomekm147@gmail.com>
2020-05-25 20:56:20 +0200

    Changed executing of multicall and added UTs for some api modules

---
+++ tests/test_api.py
@@ -0,0 +1,85 @@
+from unittest.mock import call
+
+import pytest
+
+from src.api.tm_requests import XmlRpc, RpcMulticall
+from src.api.tm_types import Status
+
+
+@pytest.fixture
+def transport(mocker):
+    return mocker.patch('src.api.tm_requests.Transport')
+
+
+@pytest.fixture
+def rpc_multicall(mocker):
+    return mocker.patch('src.api.tm_requests.RpcMulticall')
+
+
+@pytest.fixture
+def multicall(mocker):
+    return mocker.patch('src.api.tm_requests.MultiCall')
+
+
+@pytest.fixture
+def method(mocker):
+    return mocker.patch('src.api.tm_requests.Method')
+
+
+def test_should_create_rpc_object(transport, rpc_multicall):
+    XmlRpc(transport.return_value)
+    rpc_multicall.assert_not_called()
+
+
+def test_should_create_rpc_as_multicall(transport, rpc_multicall):
+    rpc = XmlRpc(transport.return_value, True)
+    rpc_multicall.assert_called_once_with(rpc)
+
+
+def test_should_getattr_from_method_on_default_rpc_call(method, transport):
+    rpc = XmlRpc(transport.return_value)
+    rpc.get_status()
+
+    method.assert_called_once_with(transport.return_value, 'GetStatus')
+    method.return_value.assert_called_once()
+
+
+def test_should_getattr_from_multicall_when_multicall_requested(method, transport, rpc_multicall):
+    rpc = XmlRpc(transport.return_value, True)
+    rpc.get_status()
+
+    method.assert_not_called()
+    rpc_multicall.assert_called_once_with(rpc)
+    rpc_multicall.return_value.GetStatus.assert_called_once()
+
+
+def test_should_exec_multicall_when_multicall_requested_only(transport, multicall):
+    rpc = XmlRpc(transport.return_value, True)
+    rpc.get_status()
+
+    multicall.return_value.return_value = []
+    dummy_param = None
+    rpc.exec_multicall(dummy_param)
+
+    multicall.assert_called_once_with(rpc)
+    multicall.return_value.GetStatus.assert_called_once()
+    multicall.return_value.assert_called_once()
+
+
+@pytest.mark.parametrize('types, methods, return_values', [
+    ([Status, Status, Status], ['get_status', 'get_status', 'get_status'], [{'Code': 1, 'Name': 'name1'},
+                                                                            {'Code': 2, 'Name': 'name2'},
+                                                                            {'Code': 3, 'Name': 'name3'}]),
+    ([Status, bool, int], ['get_status', 'start_server_lan', 'get_referee_mode'], [{'Code': 1, 'Name': 'name1'},
+                                                                                   True, 1337])])
+def test_exec_multicall_returns_only_dicts(types, methods, return_values, transport, multicall):
+    rpc = XmlRpc(transport.return_value, True)
+    [getattr(rpc, method)() for method in methods]
+    multicall.return_value.return_value = return_values
+
+    results = rpc.exec_multicall(*types)
+    multicall.return_value.assert_called_once()
+
+    for type_name, return_value, result in zip(types, return_values, results):
+        assert type_name(return_value) == result
+
